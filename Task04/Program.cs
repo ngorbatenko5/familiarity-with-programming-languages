b5bc4ca185f3657d89d908b325f97c20b1008c30
Nikita <gorbatenko.nik@bk.ru>
2023-03-01 03:21:58 +0700

    Выыод послежней цифры трехначного числа

---
+++ Task04/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трёхзначное целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastDigit = number % 10;
+Console.WriteLine($"Последняя цифра: {lastDigit}");
+
