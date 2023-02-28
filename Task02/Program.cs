4bfdadf23c02cf54b58753c4bbe9c94c807ca5e8
Nikita <gorbatenko.nik@bk.ru>
2023-03-01 03:15:16 +0700

    Дни недели по цифре

---
+++ Task02/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите число дня недели:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (number1 == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (number1 == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (number1 == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (number1 == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (number1 == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (number1 == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Неверное число недели:");
+}
+
