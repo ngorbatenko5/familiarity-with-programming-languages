c02157500f63e83d987b65c2d10a7e821a13cfb3
Nikita <gorbatenko.nik@bk.ru>
2023-03-01 03:18:43 +0700

    Диапозон числа N от - N до N

---
+++ Task03/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write(count+ " ");
+        count++;
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели ненатуральное число:");
+}
